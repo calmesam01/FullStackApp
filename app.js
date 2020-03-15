5285e32bee51a0462e320ebf96e502a8328e1994
calmesam01 <sanyamsareen01@gmail.com>
2020-03-15 01:55:58 -0400

    Used hashing using bcrypt for password encryption

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+// const encrypt = require('mongoose-encryption');
+// const md5 = require('md5');
 
 const app = express();
 
@@ -16,21 +19,24 @@ const People = mongoose.Schema({
     password: String
 });
 
+// const secret = "SecretBaby";
+// People.plugin(encrypt, {secret: secret, encryptedFields: ["[password"]});
+
 const Person = mongoose.model("Person", People);
 
-const person1 = new Person({
-    firstName: "Sam",
-    lastName: "adas",
-    email: "dsdxs@dwsdw.vo",
-    password: "sfcdsfcdcv"
-});
+// const person1 = new Person({
+//     firstName: "Sam",
+//     lastName: "adas",
+//     email: "dsdxs@dwsdw.vo",
+//     password: "sfcdsfcdcv"
+// });
 
-const person2 = new Person({
-    firstName: "Samfced",
-    lastName: "adgvrfas",
-    email: "dsdxsrfgrsdfvrf@dwsdw.vo",
-    password: "sfcrgrt34dsfcdcv"
-});
+// const person2 = new Person({
+//     firstName: "Samfced",
+//     lastName: "adgvrfas",
+//     email: "dsdxsrfgrsdfvrf@dwsdw.vo",
+//     password: "sfcrgrt34dsfcdcv"
+// });
 
 // Person.insertMany([person1, person2], function (err) {
 //     if (err) {
@@ -67,16 +73,16 @@ app.post("/signUp", function (req, res) {
     var lname = req.body.lname;
     var email = req.body.email;
     var pwd = req.body.pwd;
-
-    person3 = new Person({
-        firstName: fname,
-        lastName: lname,
-        email: email,
-        password: pwd
+    const saltRounds = 10;
+    bcrypt.hash(pwd, saltRounds, function(err, hash) {
+        person3 = new Person({
+            firstName: fname,
+            lastName: lname,
+            email: email,
+            password: hash
+        });
+        person3.save();
     });
-
-    person3.save();
-    
     res.write("Thank you for signing up " + fname + " " + lname);
     res.write(" We have sent a verification link to " + email);
     res.end();
@@ -85,21 +91,29 @@ app.post("/signUp", function (req, res) {
 app.post("/signin", function (req, res) {
     var email2 = req.body.email;
     var pwd2 = req.body.pwd;
-    Person.find({email: email2, password: pwd2}, function (err, person) {
+    Person.findOne({email: email2}, function (err, userFound) {
         if (err) {
             console.log(err);
+            res.render('failure');
         }
         else {
-            if (person.length != 0) {
-                res.render("success");
+            if (userFound) {
+                bcrypt.compare(pwd2, userFound.password, function(err, result) {
+                    if (result === true) {
+                        res.render("success");
+                    }
+                    else {
+                        res.render("failure");
+                    }
+                });
             }
             else {
                 res.render("failure");
             }
-        }
-            
-    })
+        }               
+    });
 });
+
 
 app.listen(3000, function () {
     console.log("Server running on port 3000");
